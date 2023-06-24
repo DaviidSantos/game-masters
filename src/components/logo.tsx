9752dd2cb059412fe89875f58f3393378cde5a24
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-06-24 20:12:48 -0300

    refactor: changed the position attribute and size of logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,13 +3,15 @@ import Link from "next/link";
 
 export const Logo = () => {
   return (
-    <div className="py-3 w-11/12 mx-auto">
-      <Link
-        href={"./"}
-        className={`text-zinc-100 tracking-tighter text-xl font-black`}
-      >
-        Game Masters
-      </Link>
+    <div className="flex justify-center w-full">
+      <div className="py-3 w-10/12 mx-auto z-50 absolute">
+        <Link
+          href={"./"}
+          className={`text-zinc-100 tracking-tighter text-2xl font-black [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]`}
+        >
+          Game Masters
+        </Link>
+      </div>
     </div>
   );
 };
