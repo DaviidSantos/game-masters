fc7ded0343e9f512ed71f27f73d6807cf5fb1007
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-06-24 20:12:13 -0300

    feat: created home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-h-screen w-screen`}>
+      <body className={`${inter.className} min-h-screen w-screen bg-zinc-950`}>
         <Logo />
         {children}
       </body>
