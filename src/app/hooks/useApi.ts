7d5fc430ac481b08e43b9eeaae6c7fa112ec5db0
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-06-24 22:25:10 -0300

    feat: created useApi hook

---
+++ src/app/hooks/useApi.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function useApi<T = unknown>(url: string) {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fetchData = async () => {
+    await axios
+      .get(url, {
+        headers: { "dev-email-address": "daavid.psantos@gmail.com" },
+        timeout: 1000 * 5,
+      })
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  });
+
+  return { data, error };
+}
