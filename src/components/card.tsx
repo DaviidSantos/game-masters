ee387015f5b01f4e1162ff35e095cf4205e23782
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-06-24 22:07:55 -0300

    feat: created card component

---
+++ src/components/card.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+interface CardProps {
+  id: number;
+  title: string;
+  short_description: string;
+  thumbnail: string;
+  genre: string;
+}
+
+export const Card = ({
+  id,
+  title,
+  short_description,
+  thumbnail,
+  genre,
+}: CardProps) => {
+  return (
+    <Link href={`${id}`} className="block rounded-md overflow-hidden bg-zinc-900 max-w-[300px] h-[375px] shadow-sm shadow-black hover:scale-105 group">
+      <div className="relative">
+        <Image
+          src={thumbnail}
+          alt={`Image of the game ${title}`}
+          width={300}
+          height={200}
+          className="group-hover:opacity-90 gilt"
+        />
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-zinc-900"></div>
+      </div>
+
+      <div className="py-3 px-5">
+        <h3 className="text-zinc-100 font-medium text-lg">{title}</h3>
+
+        <p className="text-zinc-300 text-xs py-2">{short_description}</p>
+        <p className="text-zinc-300 text-xs absolute bottom-5">Genre: {genre}</p>
+      </div>
+    </Link>
+  );
+};
