691a79b9f4cb2677290bfd38c965869879e2174b
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-06-25 00:44:00 -0300

    feat: created loading spinner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import { Logo } from "@/components/logo";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Suspense } from "react";
+import Image from "next/image";
+import gamesImage from "@/assets/home-image.png";
+import Loading from "./loading";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +20,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-h-screen w-screen bg-zinc-950`}>
+      <body className={`${inter.className} min-h-screen bg-zinc-950`}>
         <Logo />
-        {children}
+        <div className="relative">
+          <Image
+            src={gamesImage}
+            alt="Hero image"
+            className="object-cover object-center  w-full aspect-1 sm:aspect-2"
+          />
+          <div className="absolute inset-0 bg-gradient-to-b from-transparent to-zinc-950"></div>
+        </div>
+        <Suspense fallback={<Loading />}>{children}</Suspense>
       </body>
     </html>
   );
